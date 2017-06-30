55938869dd018aa979e829d00f07570e2ab75844
Ky Ky <kyky@kyky.ky>
2017-06-30 23:09:55 +0300

    *

--- house-prices/R/NA_fix/lot_frontage_fix_na.R
+++ house-prices/R/NA_fix/lot_frontage_fix_na.R
@@ -1,14 +1,13 @@
 
 FixNaLotFrontage.Blmngtn = function (df.train) {
-  df.train %>%
-    mutate(LotFrontageCalc = ifelse(Neighborhood == 'Blmngtn', 
-                                      ifelse(LotAreaSqrt < 59, 43, 53), 
-                                      LotFrontageCalc))
+  df.train[df.train$Neighborhood == 'Blmngtn' & df.train$LotAreaSqrt < 59, "LotFrontageCalc"] = 43
+  df.train[df.train$Neighborhood == 'Blmngtn' & df.train$LotAreaSqrt >= 59, "LotFrontageCalc"] = 53
+  df.train
 }
 
 FixNaLotFrontage.BrkSide = function (df.train, df.data.BrkSide) {
   
-  df.train.BrkSide = df.train %>% filter(Neighborhood == 'BrkSide')
+  df.train.BrkSide    = df.train %>% filter(Neighborhood == 'BrkSide') 
   df.train.BrkSide.RL = df.train.BrkSide %>% filter(MSZoning == "RL")
   df.train.BrkSide.RM = df.train.BrkSide %>% filter(MSZoning == "RM")
   
@@ -16,25 +15,13 @@ FixNaLotFrontage.BrkSide = function (df.train, df.data.BrkSide) {
   df.data.BrkSide.RM = df.data.BrkSide %>% filter(MSZoning == "RM")
   
   lm.BrkSide.RL = lm(LotFrontage ~ LotAreaSqrt, data = df.data.BrkSide.RL)
+  
   median.RM = (df.data.BrkSide.RM %>% 
                   select(LotFrontage) %>% 
                   summarise(median=median(LotFrontage))
               )$median
   
-  df.train %>%
-    mutate(LotFrontageCalc = 
-             ifelse( Neighborhood == 'BrkSide', 
-                                
-                  ifelse( MSZoning == "RL",
-                      
-                      predict(lm.BrkSide.RL, df.train.BrkSide.RL), 
-                      
-                      ifelse( MSZoning == "RM",
-                          median.RM,
-                          LotFrontageCalc
-                      )
-                  ),
-                  LotFrontageCalc
-             )
-    )
+  df.train[df.train$Neighborhood == 'BrkSide' & df.train$MSZoning == 'RL', "LotFrontageCalc"] = predict(lm.BrkSide.RL, df.train.BrkSide.RL)
+  df.train[df.train$Neighborhood == 'BrkSide' & df.train$MSZoning == 'RM', "LotFrontageCalc"] = median.RM
+  df.train
 }
