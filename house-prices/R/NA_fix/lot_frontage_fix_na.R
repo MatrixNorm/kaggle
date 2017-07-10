2a172910b43e0ab3e0029bf70f77cb2bdf45e042
Ky Ky <kyky@kyky.ky>
2017-07-10 03:23:28 +0300

    *

--- house-prices/R/NA_fix/lot_frontage_fix_na.R
+++ house-prices/R/NA_fix/lot_frontage_fix_na.R
@@ -1,12 +1,10 @@
 
 
-universalFixer = function (patchMaker) {
+fixerMakerFactory = function (patchMaker) {
   
   function (condition) {
-    #condition_call = substitute(condition)
-    
+
     function (df.train, df.data) {
-      #browser()
       df.data.subset  = df.data [ eval(condition, df.data), ]
       df.train.subset = df.train[ eval(condition, df.train), ]
 
@@ -20,28 +18,20 @@ universalFixer = function (patchMaker) {
   }
 }
 
-
-simpleLmFixerMaker = universalFixer(function (data) {
+simpleLmFixerMaker = fixerMakerFactory(function (data) {
   lm(LotFrontage ~ LotAreaSqrt, data = data)
 })
 
-medianFixerMaker = universalFixer(function (data) {
+medianFixerMaker = fixerMakerFactory(function (data) {
   median(data$LotFrontage)
 })
 
-
-meanFixerMaker = universalFixer(function (data) {
+meanFixerMaker = fixerMakerFactory(function (data) {
   mean(data$LotFrontage)
 })
 
-FixNaLotFrontage.Blmngtn = function (df.train) {
-  df.train[ eval( substitute(Neighborhood == 'Blmngtn' & LotAreaSqrt < 59), df.train), "LotFrontageCalc"] = 43
-  df.train[ eval( substitute(Neighborhood == 'Blmngtn' & LotAreaSqrt >= 59), df.train), "LotFrontageCalc"] = 53
-  df.train
-}
 
-
-qwerty = function (...) {
+multyFixerFactory = function (...) {
   # cond1, fixerMAker1, cond2, fixermaker2, ...
   # e.g.
   # MSZoning == "RL", simpleLmFixerMaker, MSZoning == "RM", medianFixerMaker
@@ -54,7 +44,6 @@ qwerty = function (...) {
       
       condition = args[[2*i - 1]]
       fixerMaker = args[[2*i]]
-      #browser()
       df.data.slice = df.data %>% filter(eval(condition))
       fixer = eval(fixerMaker)(condition)
       
@@ -64,69 +53,12 @@ qwerty = function (...) {
   }
 }
 
-FixNaLotFrontage.BrkSide = qwerty(Neighborhood == 'BrkSide' & MSZoning == "RL", simpleLmFixerMaker, 
-                                  Neighborhood == 'BrkSide' & MSZoning == "RM", medianFixerMaker)
 
-# FixNaLotFrontage.BrkSide = function (df.train, df.data.BrkSide) {
-#   
-#   df.data.BrkSide.RL = df.data.BrkSide %>% filter(MSZoning == "RL")
-#   df.data.BrkSide.RM = df.data.BrkSide %>% filter(MSZoning == "RM")
-#   
-#   FixRL = simpleLmFixerMaker(Neighborhood == 'BrkSide' & MSZoning == "RL")
-#   FixRM = medianFixerMaker(Neighborhood == 'BrkSide' & MSZoning == "RM")
-#   
-#   df.train = FixRL(df.train, df.data.BrkSide.RL)
-#   df.train = FixRM(df.train, df.data.BrkSide.RM)
-#   df.train
-# }
-
-
-FixNaLotFrontage.ClearCr = function (df.train, df.data) {
-  
-  df.data.Reg = df.data %>% filter(LotShape2 == "Reg")
-  df.data.Ireg = df.data %>% filter(LotShape2 != "Reg")
-  
-  FixReg = meanFixerMaker(Neighborhood == 'ClearCr' & LotShape2 == "Reg")
-  FixIreg = meanFixerMaker(Neighborhood == 'ClearCr' & LotShape2 != "Reg")
-  
-  df.train = FixReg(df.train, df.data.Reg)
-  df.train = FixIreg(df.train, df.data.Ireg)
+FixNaLotFrontage.Blmngtn = function (df.train) {
+  df.train[ eval( substitute(Neighborhood == 'Blmngtn' & LotAreaSqrt < 59), df.train), "LotFrontageCalc"] = 43
+  df.train[ eval( substitute(Neighborhood == 'Blmngtn' & LotAreaSqrt >= 59), df.train), "LotFrontageCalc"] = 53
   df.train
 }
-
-
-FixNaLotFrontage.CollgCr = function (df.train, df.data) {
-  
-  df.data.Reg  = df.data %>% filter(LotShape2 == "Reg")
-  df.data.Ireg = df.data %>% filter(LotShape2 == "Ireg")
-  
-  FixReg = simpleLmFixerMaker(Neighborhood == 'CollgCr' & LotShape2 == "Reg")
-  FixIreg = medianFixerMaker(Neighborhood == 'CollgCr' & LotShape2 == "Ireg")
-  
-  df.train = FixReg(df.train, df.data.Reg)
-  df.train = FixIreg(df.train, df.data.Ireg)
-  df.train
-}
-
-
-FixNaLotFrontage.Crawfor = function (df.train, df.data) {
-  
-  df.data.Reg  = df.data %>% filter(LotShape2 == "Reg")
-  df.data.Ireg = df.data %>% filter(LotShape2 == "Ireg")
-  
-  FixReg = simpleLmFixerMaker(Neighborhood == 'Crawfor' & LotShape2 == "Reg")
-  FixIreg = simpleLmFixerMaker(Neighborhood == 'Crawfor' & LotShape2 == "Ireg")
-  
-  df.train = FixReg(df.train, df.data.Reg)
-  df.train = FixIreg(df.train, df.data.Ireg)
-  df.train
-}
-
-
-FixNaLotFrontage.Edwards = simpleLmFixerMaker(Neighborhood == 'Edwards')
-FixNaLotFrontage.Gilbert = simpleLmFixerMaker(Neighborhood == 'Gilbert')
-FixNaLotFrontage.IDOTRR  = simpleLmFixerMaker(Neighborhood == 'IDOTRR')
-
 
 FixNaLotFrontage.MeadowV = function (df.train) {
   df.train[df.train$Neighborhood == 'MeadowV' & df.train$LotAreaSqrt < 47, "LotFrontageCalc"] = 21
@@ -134,3 +66,37 @@ FixNaLotFrontage.MeadowV = function (df.train) {
   df.train[df.train$Neighborhood == 'MeadowV' & df.train$LotAreaSqrt >= 51, "LotFrontageCalc"] = 41
   df.train
 }
+
+FixNaLotFrontage.NPkVill = function (df.train) {
+  df.train[ eval( substitute(Neighborhood == 'NPkVill' & LotAreaSqrt < 55), df.train), "LotFrontageCalc"] = 23
+  df.train[ eval( substitute(Neighborhood == 'NPkVill' & LotAreaSqrt >= 55), df.train), "LotFrontageCalc"] = 53
+  df.train
+}
+
+
+FixNaLotFrontage.BrkSide = multyFixerFactory(
+    Neighborhood == 'BrkSide' & MSZoning == "RL", simpleLmFixerMaker, 
+    Neighborhood == 'BrkSide' & MSZoning == "RM", medianFixerMaker)
+
+FixNaLotFrontage.ClearCr = multyFixerFactory(
+    Neighborhood == 'ClearCr' & LotShape2 == "Reg", meanFixerMaker, 
+    Neighborhood == 'ClearCr' & LotShape2 != "Reg", meanFixerMaker)
+
+FixNaLotFrontage.CollgCr = multyFixerFactory(
+  Neighborhood == 'CollgCr' & LotShape2 == "Reg", simpleLmFixerMaker, 
+  Neighborhood == 'CollgCr' & LotShape2 == "Ireg", medianFixerMaker)
+
+FixNaLotFrontage.Crawfor = multyFixerFactory(
+  Neighborhood == 'Crawfor' & LotShape2 == "Reg", simpleLmFixerMaker, 
+  Neighborhood == 'Crawfor' & LotShape2 == "Ireg", simpleLmFixerMaker)
+
+FixNaLotFrontage.NoRidge = multyFixerFactory(
+  Neighborhood == 'NoRidge' & LotShape2 == "Reg", simpleLmFixerMaker, 
+  Neighborhood == 'NoRidge' & LotShape2 == "Ireg", simpleLmFixerMaker)
+
+
+FixNaLotFrontage.Edwards = simpleLmFixerMaker(substitute(Neighborhood == 'Edwards'))
+FixNaLotFrontage.Gilbert = simpleLmFixerMaker(substitute(Neighborhood == 'Gilbert'))
+FixNaLotFrontage.IDOTRR  = simpleLmFixerMaker(substitute(Neighborhood == 'IDOTRR'))
+FixNaLotFrontage.Mitchel  = simpleLmFixerMaker(substitute(Neighborhood == 'Mitchel'))
+
