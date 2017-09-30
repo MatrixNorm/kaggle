6cfb0fafcacaffd288724c07b2521281f1119caf
Ky Ky <kyky@kyky.ky>
2017-10-01 00:55:51 +0300

    *

--- house-prices/R/utils.R
+++ house-prices/R/utils.R
@@ -1,4 +1,17 @@
 
+source('./helpers.R')
+
+
+kaggle.house <- within(kaggle.house, 
+{
+    
+    utils <- within(list(), 
+    {
+        get_char_columns_names <- function (df) {
+            df %>% purrr::map(~is.character(.)) %>% purrr::keep(~.) %>% names %>% sort
+        }
+    })
+})
 
 # XXX
 utils.heterogeneity_score = function(vector){
