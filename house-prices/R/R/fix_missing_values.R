1f42d3ac5d1482890ed8751a53dc0b9452909bb7
Ky Ky <kyky@kyky.ky>
2017-10-27 02:00:27 +0300

    *

--- house-prices/R/R/fix_missing_values.R
+++ house-prices/R/R/fix_missing_values.R
@@ -24,9 +24,34 @@ na <- within(list(),
         registerFixer("Exterior2nd",  replace_na_with_value, 'VinylSd')
         registerFixer("Functional",   replace_na_with_value, 'Typ')
         registerFixer("KitchenQual",  replace_na_with_value, 'TA')
+        registerFixer("MSZoning",  replace_na_with_value, 'RL')
+        registerFixer("SaleType",  replace_na_with_value, 'Oth')
         registerFixer("SaleType",  replace_na_with_value, 'WD')
         
+        MasVnrType <- function (df) {
+            df[  is.na(df$MasVnrType) &
+                     !is.na(df$MasVnrArea) & df$MasVnrArea > 0, "MasVnrType"] <- 'BrkFace'
+            
+            df[ ( !is.na(df$MasVnrArea) & df$MasVnrArea == 0 ) &
+                    ( is.na(df$MasVnrType) | df$MasVnrType != 'None' ), "MasVnrType"] <- 'None'
+            
+            df[is.na(df$MasVnrType), "MasVnrType"] <- 'None'
+            
+            df
+        }
+        
         # numeric
+        for (attr in c('BedroomAbvGr', 'BsmtFinSF1', 'BsmtFinSF2', 'BsmtFullBath', 
+                       'BsmtHalfBath', 'BsmtUnfSF', 'EnclosedPorch', 'Fireplaces',
+                       'FullBath', 'GarageArea', 'GarageCars', 'GarageYrBlt',
+                       'GrLivArea', 'HalfBath', 'KitchenAbvGr', 'LotArea', 'LotFrontage',
+                       'LowQualFinSF', 'MasVnrArea', 'MiscVal', 'MoSold', 'OpenPorchSF',
+                       'OverallCond', 'OverallQual', 'PoolArea', 'SalePrice', 'ScreenPorch',
+                       'TotalBsmtSF', 'TotRmsAbvGrd', 'WoodDeckSF', 'X1stFlrSF', 'X2ndFlrSF', 
+                       'X3SsnPorch', 'YearBuilt', 'YearRemodAdd', 'YrSold')) 
+        {
+            
+        }
         registerFixer("BsmtFinSF1",   replace_na_with_zero)
         registerFixer("BsmtFinSF2",   replace_na_with_zero)
         registerFixer("BsmtFullBath", replace_na_with_zero)
@@ -44,21 +69,6 @@ na <- within(list(),
 
             df
         }
-
-        MasVnrType <- function (df) {
-            df[  is.na(df$MasVnrType) &
-                !is.na(df$MasVnrArea) & df$MasVnrArea > 0, "MasVnrType"] <- 'BrkFace'
-
-            df[ ( !is.na(df$MasVnrArea) & df$MasVnrArea == 0 ) &
-                ( is.na(df$MasVnrType) | df$MasVnrType != 'None' ), "MasVnrType"] <- 'None'
-
-            df[is.na(df$MasVnrType), "MasVnrType"] <- 'None'
-
-            df
-        }
-
-        registerFixer("MSZoning",  replace_na_with_value, 'RL')
-        registerFixer("SaleType",  replace_na_with_value, 'Oth')
 
         TotalBsmtSF <- function (df) {
             df[is.na(df$TotalBsmtSF) & is.na(df$BsmtCond), "TotalBsmtSF"] <- 0
