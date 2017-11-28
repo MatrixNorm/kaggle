edcb73c406eb0248170f6308862cdc08d01250c5
Ky Ky <kyky@kyky.ky>
2017-11-29 00:40:15 +0300

    *

--- house-prices/R/R/transform_vars.R
+++ house-prices/R/R/transform_vars.R
@@ -59,43 +59,4 @@ trans <- within(list(),
         spread(var.name, avg_)
     }
     
-    # averagingTransform2 <- function (trainset, testset, y.var, id.var, stat.fun=mean, diff=F) {
-    # 
-    #     y.var <- enquo(y.var)
-    #     id.var <- enquo(id.var)
-    # 
-    #     train.long <- 
-    #         trainset %>%
-    #         gather(var.name, var.value, -!!y.var, -!!id.var) %>%
-    #         mutate(src = 'train')
-    #     
-    #     test.long <-
-    #         testset %>%
-    #         gather(var.name, var.value, -!!id.var) %>%
-    #         mutate(src = 'test')
-    #     
-    #     long <- 
-    #         train.long %>% 
-    #         bind_rows(test.long) %>%
-    #         group_by(var.name, var.value) %>%
-    #         arrange(var.name, var.value) %>%
-    #         mutate(avg_ = stat.fun(!!y.var, na.rm=T)) %>%
-    #         group_by(var.name) %>%
-    #         mutate(avg_ = ifelse(is.na(avg_), stat.fun(!!y.var, na.rm=T), avg_))
-    #     
-    #     if ( diff ) {
-    #         long <- long %>% mutate(avg_ = avg_ - stat.fun(!!y.var, na.rm=T))
-    #     }
-    #     
-    #     wide <-
-    #         long %>% 
-    #         select(-var.value) %>% 
-    #         spread(var.name, avg_)
-    #     
-    #     list(
-    #         trainset = wide %>% filter(src == "train") %>% select(-src),
-    #         testset = wide %>% filter(src == "test") %>% select(-src, -!!y.var)
-    #     )
-    # }
- 
 })
