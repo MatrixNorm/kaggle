ed56c2ca813e2fdb1f4e370ee341c293a8c8dffb
Ky Ky <kyky@kyky.ky>
2017-12-07 02:02:22 +0300

    *

--- house-prices/R/R/plot.R
+++ house-prices/R/R/plot.R
@@ -1,7 +1,7 @@
 
 plot <- within(list(), 
 {
-    jitter <- function (data, x.var, y.var, color.var) {
+        jitter <- function (data, x.var, y.var, color.var) {
         data %>%
             ggplot(aes_string(x=x.var, y=y.var)) +
             geom_jitter(aes_string(color=color.var), alpha=0.3, width=0.1, height=0) +
@@ -10,7 +10,7 @@ plot <- within(list(),
             theme(legend.position="bottom")
     } 
     
-    qq <- function (df, var.name) {
+    qq <- function (df, var.name, alpha=0.2) {
         
         var.name <- enquo(var.name)
         var.name.char <- as.character(var.name)[2]
@@ -24,7 +24,7 @@ plot <- within(list(),
             normed := (var.vector - mean(!!var.name)) / sd(!!var.name)
         ) %>%
         ggplot() +
-        geom_qq(aes(sample=normed), alpha=0.2) +
+        geom_qq(aes(sample=normed), alpha=alpha) +
         geom_abline(slope=1) +
         theme_bw()
     }
