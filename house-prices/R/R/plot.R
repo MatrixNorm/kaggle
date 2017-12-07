5d4e8aa5740e2fd26f70c2fbdc3bd2c672a39602
Ky Ky <kyky@kyky.ky>
2017-12-08 02:01:32 +0300

    *

--- house-prices/R/R/plot.R
+++ house-prices/R/R/plot.R
@@ -29,4 +29,69 @@ plot <- within(list(),
         theme_bw()
     }
    
+    compareFormulas <- function (formulas, targer.var) {
+        
+        targer.var <- enquo(targer.var)
+        targer.var.char <- as.character(targer.var)[[2]]
+        
+        X <- 
+            tibble(
+                formula = formulas
+            ) %>%
+            mutate(
+                mod = map(formula, ~lm(as.formula(.), df)),
+                glance = map(mod, broom::glance),
+                augment = map(mod, broom::augment),
+                r2 = map_dbl(glance, 'r.squared')
+            )
+        
+        X.wide <-
+            X %>% 
+            select(formula, augment) %>% 
+            unnest %>% 
+            select(formula, !!targer.var, fitted=.fitted, resid=.resid) %>%
+            group_by(formula) %>%
+            mutate(
+                resid.normed = (resid - mean(resid)) / sd(resid)
+            )
+        
+        X.long <-
+            X %>% 
+            select(formula, augment) %>% 
+            unnest %>% 
+            select(-.se.fit, -.hat, -.sigma, -.cooksd, -.std.resid) %>%
+            rename(fitted=.fitted, resid=.resid) %>%
+            gather(name, value, -formula, -!!targer.var, -fitted, -resid) %>%
+            filter(!is.na(value)) %>%
+            group_by(formula, name)
+        
+        resid_qq <-
+            X.wide %>%
+            ggplot(aes(sample=resid.normed, color=formula)) +
+            geom_qq(alpha=0.4) +
+            geom_abline(slope=1) +
+            theme_bw() +
+            theme(legend.position="bottom", legend.direction="vertical")
+
+        resid_vs_target <-
+            X.wide %>%
+            ggplot(aes_string(x=targer.var.char, y="resid", color="formula")) +
+            geom_point(alpha=0.7, shape='o', size=2) +
+            geom_hline(yintercept=0) +
+            theme_bw() +
+            theme(legend.position="bottom", legend.direction="vertical")
+
+        actual_vs_predicted <-
+            X.wide %>%
+            ggplot(aes_string(x=targer.var.char, y="fitted", color="formula")) +
+            geom_point(alpha=0.7, shape='o', size=2) +
+            geom_abline(slope=1, color="red") +
+            theme_bw() +
+            theme(legend.position="bottom", legend.direction="vertical")
+
+        grob1 <- arrangeGrob(resid_qq, resid_vs_target, actual_vs_predicted,
+                             layout_matrix=rbind(c(1, 2, 3)))
+        
+        list(X=X, X.long=X.long, X.wide=X.wide, grob1=grob1)    
+    }
 })
