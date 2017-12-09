cb6a782e4bd748a0c1efa19aa9cb216c102efe7d
Ky Ky <kyky@kyky.ky>
2017-12-09 03:33:27 +0300

    *

--- house-prices/R/R/validate.R
+++ house-prices/R/R/validate.R
@@ -1,100 +1,78 @@
 
 validate <- within(list(), 
 {
-    trainMany.old <- function (dataset, N=5, sample.share=0.75, trainset.share=0.5, modelFactory) {
+    trainMany.old <- function (dataset, target.var, N=5, trainset.share=0.5, modelFactory, transformFactory) {
+        
         c(1:N) %>% map(function (i) {
             
-            sample = dataset
-            if ( sample.share != 1 ) {
-                sample <- dataset %>% sample_n(round(sample.share * nrow(dataset)))
-            }
-            sample.partition.index <- caret::createDataPartition(y=sample$sale_price_log, 
-                                                                 p=trainset.share, list=F, times=1)
+            partition.index <- caret::createDataPartition(y=dataset %>% `$`(target.var), 
+                                                            p=trainset.share, list=F, times=1)
             
-            trainset <- sample[sample.partition.index,] %>% select(-SalePrice)
-            testset <- sample[-sample.partition.index,] %>% select(-sale_price_log)
+            trainset <- sample[partition.index,]
+            testset <- sample[-partition.index,]
             
-            stopifnot(setdiff(trainset %>% colnames, testset %>% colnames) == 'sale_price_log')
-            stopifnot(setdiff(testset %>% colnames, trainset %>% colnames) == 'SalePrice')
+            target_test_actual <- testset %>% select(one_of(target.var))
+            testset <- testset %>% select(-one_of(target.var))
             
-            tranform.results <- kaggle.house$trans$doItAll(trainset, testset)
-            trainset <- tranform.results$trainset
-            testset <- tranform.results$testset
+            res <- transformFactory(trainset, testset)
+            trainset.after.tran <- res$trainset
+            testset.after.tran <- res$testset
             
-            stopifnot(0 == trainset %>% purrr::map(function (col) { !is.numeric(col) }) %>% unlist %>% sum)
-            stopifnot(0 == testset %>% purrr::map(function (col) { !is.numeric(col) }) %>% unlist %>% sum)
-
-            model <- modelFactory(trainset)
-            y_predicted <- predict(model, testset %>% select(-SalePrice)) %>% as.vector
-            y_actual <- testset %>% mutate(sale_price_log = log(SalePrice)) %>% select(sale_price_log) %>% `[[`(1)
+            model <- modelFactory(trainset.after.tran)
             
-            list(model=model, y_test_predicted=y_predicted, y_test_actual=y_actual)
+            target_test_predicted <- predict(model, testset.after.tran) %>% as.vector
+ 
+            list(model=model, target_test_predicted=target_test_predicted, target_test_actual=target_test_actual)
         })
     }
     
-    trainAndTest <- function(dataset, y.var, sample.share=1, trainset.share=0.5, modelFactory, transformFactory) {
+    trainAndTest <- function(dataset, target.var, trainset.share=0.5, modelFactory, transformFactory=NULL) {
         
-        sample = dataset
-        if ( sample.share < 1 ) {
-            sample <- dataset %>% sample_n(round(sample.share * nrow(dataset)))
-        }
-        sample.partition.index <- caret::createDataPartition(y=sample[,y.var] %>% `[[`(1), 
-                                                             p=trainset.share, list=F, times=1)
+        partition.index <- caret::createDataPartition(y=dataset[,target.var] %>% `[[`(1), 
+                                                      p=trainset.share, list=F, times=1)
         
-        trainset <- sample[sample.partition.index,]
-        testset  <- sample[-sample.partition.index,] %>% select(-one_of(y.var))
+        trainset <- dataset[partition.index,]
+        testset <- dataset[-partition.index,]
         
-        stopifnot(setdiff(trainset %>% colnames, testset %>% colnames) == y.var)
+        target_test_actual <- testset %>% select(one_of(target.var)) %>% `[[`(1)
+        testset <- testset %>% select(-one_of(target.var))
+        
+        stopifnot(setdiff(trainset %>% colnames, testset %>% colnames) == target.var)
         stopifnot(setdiff(testset %>% colnames, trainset %>% colnames) == '')
         
         trainset.ready <- trainset
         testset.ready  <- testset
-        if ( !missing(transformFactory) ) {
-            tranform.results <- transformFactory(trainset, testset)
-            trainset.ready <- tranform.results$trainset
-            testset.ready <- tranform.results$testset        
+        if ( !is.null(transformFactory) ) {
+            res <- transformFactory(trainset, testset)
+            trainset.ready <- res %>% filter(dataSource == "train") %>% select(-dataSource)
+            testset.ready <- res %>% filter(dataSource == "test") %>% select(-dataSource)
         }
         
         model <- modelFactory(trainset.ready)
+        
         test.results = tibble(
-            actual = sample[-sample.partition.index, y.var] %>% `[[`(y.var),
+            actual = target_test_actual,
             predicted = predict(model, testset.ready) %>% as.vector
         )
 
-        list(model=model, test.results=test.results)
+        tibble(model=list(model), test.results=list(test.results))
     }
     
-    trainAndTestMany <- function(dataset, y.var, N, sample.share=0.75, trainset.share=0.5, modelFactory, transformFactory) {
-        
-        is.transformFactory.missing <- missing(transformFactory)
+    trainAndTestMany <- function(dataset, target.var, N, trainset.share=0.5, modelFactory, transformFactory=NULL) {
         
         fits <- c(1:N) %>% map(function (i) {
-            # XXX add better handling of missing transformFactory
-            if ( !is.transformFactory.missing ) {
-                fit <- kaggle.house$validate$trainAndTest(
-                    dataset=dataset, 
-                    y.var=y.var, 
-                    sample.share=sample.share, 
-                    trainset.share=trainset.share, 
-                    modelFactory=modelFactory,
-                    transformFactory=transformFactory
-                )
-            } else {
-                fit <- kaggle.house$validate$trainAndTest(
-                    dataset=dataset, 
-                    y.var=y.var, 
-                    sample.share=sample.share, 
-                    trainset.share=trainset.share, 
-                    modelFactory=modelFactory
-                )
-            }
+
+            fit <- trainAndTest(
+                dataset=dataset, 
+                target.var=target.var, 
+                trainset.share=trainset.share, 
+                modelFactory=modelFactory,
+                transformFactory=transformFactory
+            )
             
-            fit$test.results <- fit$test.results %>% mutate(run.no = i)
+            fit$iteration.num <- i
             fit
         })
-        list(
-            models = fits %>% map(function (fit) { fit$model }),
-            tests = fits %>% map(function(item) { item$test.results }) %>% bind_rows
-        )
+        bind_rows(fits)
     }
 })
