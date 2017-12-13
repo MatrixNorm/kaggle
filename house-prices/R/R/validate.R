7025b86a6bffe3964d2c66faf10b6e16072e01bd
Ky Ky <kyky@kyky.ky>
2017-12-14 00:45:37 +0300

    *

--- house-prices/R/R/validate.R
+++ house-prices/R/R/validate.R
@@ -61,75 +61,50 @@ validate <- within(list(),
     trainAndTestMany <- function(dataset, target.var, N, trainset.share=0.5, modelFactory, transformFactory=NULL) {
         
         fits <- c(1:N) %>% map(function (i) {
-
-            fit <- trainAndTest(
+            trainAndTest(
                 dataset=dataset, 
                 target.var=target.var, 
                 trainset.share=trainset.share, 
                 modelFactory=modelFactory,
                 transformFactory=transformFactory
             )
-            
-            fit$iteration.num <- i
-            fit
         })
-        bind_rows(fits)
+        bind_rows(fits) %>%
+        mutate(
+            glance = map(model, broom::glance),
+            
+            tidy = map(model, broom::tidy),
+            
+            L2.test.score = map_dbl(test.results, function (df) {
+                sqrt(sum((df$actual - df$predicted)^2) / nrow(df))
+            }),
+            
+            L2.train.score = map_dbl(model, function (mod) {
+                target.var.name <- as.list(mod$call)$formula %>% as.list %>% `[[`(2) %>% as.character
+                augment = broom::augment(mod)
+                sqrt(sum((augment[,target.var.name] - augment$.fitted)^2) / nrow(augment))
+            }),
+            
+            R2 = map_dbl(model, function (mod) {
+                summary(mod)$r.squared
+            })
+        )
     }
     
-    exploreTrainAndTestResults <- function (results) {
-        r2 <-
-            results %>% 
-            select(model) %>%
-            mutate(
-                glance = map(model, broom::glance)
-            ) %>%
-            select(-model) %>%
-            unnest
+    plot.terms <- function (simulation.results, bins=15, facet.cols=3) {
         
-        coeffs <-
-            results %>% 
-            select(model) %>%
-            mutate(
-                tidy = map(model, broom::tidy)
-            ) %>%
-            select(-model) %>%
-            unnest %>%
-            arrange(term)
-        
-        scores <-
-            results %>%
-            mutate(
-                L2.test.score = map_dbl(test.results, function (df) {
-                    sqrt(sum((df$actual - df$predicted)^2) / nrow(df))
-                }),
-                L2.train.score = map_dbl(model, function (mod) {
-                    target.var.name <- as.list(mod$call)$formula %>% as.list %>% `[[`(2) %>% as.character
-                    augment = broom::augment(mod)
-                    sqrt(sum((augment[,target.var.name] - augment$.fitted)^2) / nrow(augment))
-                }),
-                R2 = map_dbl(model, function (mod) {
-                    summary(mod)$r.squared
-                })
-            ) %>% 
-            select(-model, -test.results)
-        
-
-        list(r2=r2, coeffs=coeffs, scores=scores)
-    }
-    
-    plotCoeffs <- function (coeffs, bins=15, facet.cols=3) {
+        terms <- simulation.results %>% select(tidy) %>% unnest %>% group_by(term)
         
         hist <-
-            coeffs %>%
+            terms %>%
             ggplot() +
             geom_histogram(aes(estimate, y=..density..), bins=bins, alpha=0.5) +
             geom_density(aes(estimate), color="blue") +
             facet_wrap(~term, ncol=facet.cols, scales="free") +
             theme_bw()
         
-        
         qq <-
-            coeffs %>%
+            terms %>%
             group_by(term) %>%
             mutate(estimate.normed = (estimate - mean(estimate)) / sd(estimate)) %>%
             ggplot() +
@@ -139,5 +114,56 @@ validate <- within(list(),
             theme_bw()
         
         list(hist=hist, qq=qq)
+    }
+    
+    plot.scores <- function (simulation.results, bins=15, facet.cols=3) {
+        
+        hist <-
+            scores %>%
+            gather(name, val) %>%
+            group_by(name) %>%
+            ggplot() +
+            geom_histogram(aes(val, y=..density..), bins=bins, alpha=0.5) +
+            geom_density(aes(val), color="blue") +
+            facet_wrap(~name, ncol=facet.cols, scales="free") +
+            theme_bw()
+        
+        qq <-
+            scores %>%
+            gather(name, val) %>%
+            group_by(name) %>%
+            mutate(val.normed = (val - mean(val)) / sd(val)) %>%
+            ggplot() +
+            geom_qq(aes(sample=val.normed), alpha=0.4) +
+            geom_abline(slope=1, color="blue") +
+            facet_wrap(~name, ncol=facet.cols, scales="free") +
+            theme_bw()
+        
+        arrangeGrob(
+            scores %>%
+                ggplot() +
+                geom_point(aes(L2.train.score, L2.test.score), alpha=0.5) +
+                theme_bw(),
+            
+            scores %>%
+                ggplot() +
+                geom_point(aes(R2, L2.train.score), alpha=0.5) +
+                theme_bw(),
+            
+            scores %>%
+                ggplot() +
+                geom_point(aes(R2, L2.test.score), alpha=0.5) +
+                theme_bw(),
+            
+            hist,
+            
+            qq,
+            
+            layout_matrix=rbind(c(1, 2, 3),
+                                c(4, 4, 4),
+                                c(5, 5, 5)), 
+            
+            widths=c(100, 100, 100)
+        )
     }
 })
