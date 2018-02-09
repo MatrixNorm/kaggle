3cb62b8766a451aea30d852dfed332a4439a1f06
Ky Ky <kyky@kyky.ky>
2018-02-09 04:52:06 +0300

    *

---
+++ ipython_config.py
@@ -0,0 +1,6 @@
+
+c = get_config()
+
+c.InteractiveShellApp.exec_lines = [
+    'import sys; sys.path.append("/mnt/hdd/develop/kaggle/python_sources")'
+]
---
+++ python_sources/kaggle/house_prices/missing.py
@@ -0,0 +1,22 @@
+
+
+colums_with_valid_na = (
+    'Alley', 
+    'BsmtCond', 
+    'BsmtExposure', 
+    'BsmtFinType1', 
+    'BsmtFinType2', 
+    'BsmtQual',
+    'Fence',
+    'FireplaceQu',
+    'GarageCond',
+    'GarageFinish',
+    'GarageQual',
+    'GarageType',
+    'MasVnrType',
+    'MiscFeature',
+    'PoolQC',
+    'Utilities',
+    'SalePrice',
+    'dataSource'
+)
