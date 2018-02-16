a5b1c0225634a8e2784861cc53c0182b4377784d
Ky Ky <kyky@kyky.ky>
2018-02-17 02:14:36 +0300

    *

--- R_sources/house_prices/attributes_selection.R
+++ R_sources/house_prices/attributes_selection.R
@@ -85,6 +85,49 @@ attributes_selection <- within(list(),
     })
     
     
+    anova <- within(list(), {
+        
+        Q_table <- function(df, target_var) {
+            target_var <- enquo(target_var)
+            target_var_char <- as.character(target_var)[2]
+            
+            df <- df %>% filter(!is.na(!!target_var))
+            
+            global_std <- sd(df[,target_var_char][[1]])
+            global_mean <- mean(df[,target_var_char][[1]])
+            
+            df %>% 
+            gather(var, value, -!!target_var) %>%
+            group_by(var, value) %>%
+            summarise(
+                n = n(),
+                mean = mean(!!target_var),
+                Q_within_group = sum( (`-`(!!target_var, mean))**2 ),
+                Q_group = n * (mean - global_mean)**2,
+                Q_total = sum( (`-`(!!target_var, global_mean))**2 )
+            ) %>%
+            group_by(var) %>%
+            summarise(
+                num_levels = n(),
+                num_observ = sum(n),
+                Q_within_groups = sum(Q_within_group),
+                Q_groups = sum(Q_group),
+                Q_total = sum(Q_total),
+                Q_control_sum = Q_groups + Q_within_groups
+            )
+        }
+        
+        arrange_vars <- function(Q_table) {
+            Q_table %>%
+            mutate(
+                F = (Q_groups / num_levels - 1) / (Q_within_groups / num_observ - num_levels)
+            ) %>%
+            select(var, F) %>%
+            arrange(F)
+        }
+    })
+    
+    
     order_factor_by_target <- function(df, factor_var, target_var) {
         factor_var <- enquo(factor_var)
         factor_var_char <- as.character(factor_var)[2]
