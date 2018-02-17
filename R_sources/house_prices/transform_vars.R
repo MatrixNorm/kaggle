e027b3abac54eadce2462109f8276161c173380c
Ky Ky <kyky@kyky.ky>
2018-02-17 23:18:07 +0300

    *

--- R_sources/house_prices/transform_vars.R
+++ R_sources/house_prices/transform_vars.R
@@ -2,49 +2,76 @@
 
 trans <- within(list(), 
 {
+    numeric <- within(list(), {
+        
+        get_transformation_config <- function(numeric_data) {
+            numeric_data %>%
+            select(-MoSold) %>%
+            gather(var, x) %>%
+            filter(!is.na(x)) %>%
+            mutate(
+                log = log(x + 1),
+                sqrt = sqrt(x)
+            ) %>%
+            gather(predictor, value, -var) %>%
+            group_by(var, predictor) %>%
+            mutate(
+                value_normed = (value - mean(value)) / sd(value)
+            ) %>%
+            group_by(var, predictor, value_normed) %>%
+            arrange(var, predictor, value_normed) %>%
+            summarise(
+                k = n()
+            ) %>%
+            mutate(
+                empirical = cumsum(k) / sum(k),
+                theoretical = pnorm(value_normed),
+                diff_L2 = k*(empirical - theoretical)**2
+            ) %>%
+            group_by(var, predictor) %>%
+            summarise(
+                L2_distance = sum(diff_L2)
+            ) %>%
+            group_by(var) %>%
+            nest %>%
+            mutate(
+                best_predictor = map(data, function(df) {
+                    best <- df %>% arrange(L2_distance) %>% head(1)
+                    x <- df %>% filter(predictor == 'x')
+                    score <- 100 * (x$L2_distance - best$L2_distance) / x$L2_distance
+                    data_frame(predictor = best$predictor, score = score)
+                })
+            ) %>%
+            select(var, best_predictor) %>%
+            unnest(best_predictor) %>%
+            filter(predictor != 'x' & score > 30)
+        }
+        
+        for_qq_plot <- function(numeric_data, transformation_config) {
+            numeric_data %>%
+            select(one_of(transformation_config$var)) %>%
+            gather(var, value) %>%
+            filter(!is.na(value)) %>%
+            inner_join(transformation_config %>% select(var, predictor), by='var') %>%
+            mutate(
+                value_transformed = recode(predictor,
+                                           log = log(value + 1),
+                                           sqrt = sqrt(value)
+                )
+            ) %>%
+            select(var, value, value_transformed) %>%
+            gather(tran, value, -var) %>%
+            mutate(
+                tran = ifelse(tran == 'value', 'vanilla', 'transformed')
+            ) %>%
+            group_by(var, tran) %>%
+            mutate(
+                normed_value = (value - mean(value)) / sd(value)
+            )
+        }
+    })
     
-    get_numeric_vars_tranformations <- function(numeric_data) {
-        numeric_data %>%
-        select(-MoSold) %>%
-        gather(var, x) %>%
-        filter(!is.na(x)) %>%
-        mutate(
-            log = log(x + 1),
-            sqrt = sqrt(x)
-        ) %>%
-        gather(predictor, value, -var) %>%
-        group_by(var, predictor) %>%
-        mutate(
-            value_normed = (value - mean(value)) / sd(value)
-        ) %>%
-        group_by(var, predictor, value_normed) %>%
-        arrange(var, predictor, value_normed) %>%
-        summarise(
-            k = n()
-        ) %>%
-        mutate(
-            empirical = cumsum(k) / sum(k),
-            theoretical = pnorm(value_normed),
-            diff_L2 = k*(empirical - theoretical)**2
-        ) %>%
-        group_by(var, predictor) %>%
-        summarise(
-            L2_distance = sum(diff_L2)
-        ) %>%
-        group_by(var) %>%
-        nest %>%
-        mutate(
-            best_predictor = map(data, function(df) {
-                best <- df %>% arrange(L2_distance) %>% head(1)
-                x <- df %>% filter(predictor == 'x')
-                score <- 100 * (x$L2_distance - best$L2_distance) / x$L2_distance
-                data_frame(predictor = best$predictor, score = score)
-            })
-        ) %>%
-        select(var, best_predictor) %>%
-        unnest(best_predictor) %>%
-        filter(predictor != 'x' & score > 30)
-    }
+    
  
     binaryTransform <- function (dataset) {
         dataset %>% 
