305f761bad2a519eacf1f1c1e4b4f0300beac2ea
Ky Ky <kyky@kyky.ky>
2018-02-20 22:33:57 +0300

    *

--- R_sources/house_prices/transform_vars.R
+++ R_sources/house_prices/transform_vars.R
@@ -88,103 +88,57 @@ trans <- within(list(),
     })
     
  
-    binaryTransform <- function (dataset) {
-        dataset %>% 
-        mutate(
-            Alley = ifelse(Alley != '_none_', 1, 0),
-            
-            CentralAir = ifelse(CentralAir == 'Y', 1, 0),
-            
-            Electrical = ifelse(Electrical == 'SBrkr', 1, 0),
-            
-            Functional = ifelse(Functional == 'Typ', 1, 0),
-            
-            Heating = ifelse(Heating == 'GasA', 1, 0),
-            
-            LandContour = ifelse(LandContour == 'Lvl', 1, 0),
-            
-            LandSlope = ifelse(LandSlope != 'Gtl', 1, 0),
-            
-            LotShape = ifelse(LotShape == 'Reg', 1, 0),
-            
-            MiscFeature = ifelse(MiscFeature != '_none_', 1, 0),
-            
-            PavedDrive = ifelse(PavedDrive == 'Y', 1, 0),
-            
-            PoolQC = ifelse(PoolQC != '_none_', 1, 0),
-            
-            RoofMatl = ifelse(RoofMatl == 'CompShg', 1, 0),
-            
-            Street = ifelse(Street == 'Pave', 1, 0),
-            
-            Utilities = ifelse(Utilities == 'AllPub', 1, 0)
-        )
-    }
-    
-    averagingTransform <- function (dataset, y.var, id.var, src.var, stat.fun=mean, diff=F) {
+    categ <- within(list(), {
         
-        y.var <- enquo(y.var)
-        id.var <- enquo(id.var)
-        src.var <- enquo(src.var)
-        
-        long <- 
-            dataset %>% 
-            gather(var.name, var.value, -!!y.var, -!!id.var, -!!src.var) %>%
-            group_by(var.name, var.value) %>%
-            mutate(avg_ = stat.fun(!!y.var, na.rm=T)) %>%
-            group_by(var.name) %>%
-            mutate(avg_ = ifelse(is.na(avg_), stat.fun(!!y.var, na.rm=T), avg_))
-        
-        if ( diff ) {
-            long <- long %>% mutate(avg_ = avg_ - stat.fun(!!y.var, na.rm=T))
-        }
-
-        long %>% 
-        select(-var.value) %>% 
-        spread(var.name, avg_)
-    }
-    
-    transformCombindDataset <- function (dataset) {
-        
-        dataset.after.binary <- dataset %>% binaryTransform
-        
-        binary.vars <- setdiff(
-            dataset %>% (kaggle.house$getCategoricalColumnNames), 
-            dataset.after.binary %>% (kaggle.house$getCategoricalColumnNames)
-        )
-        averaging.vars <- 
-            dataset.after.binary %>% 
-            select(-dataSource) %>% 
-            (kaggle.house$getCategoricalColumnNames)
-        
-        dataset.after.averaging <- 
-            averagingTransform(
-                dataset = dataset.after.binary %>% select(one_of(averaging.vars), price.log, dataSource, Id), 
-                y.var = price.log, 
-                id.var = Id,
-                src.var=dataSource,
-                stat.fun = mean, 
-                diff = TRUE
-            ) %>% select(-price.log)
-        
-        inner_join(
-            dataset.after.binary %>% select(-one_of(averaging.vars)),
-            dataset.after.averaging,
-            by=c("dataSource", "Id")
-        )
-    }
-    
-    combine.levels <- within(list(), 
-    {
-        OverallQual <- function (df) {
+        calc_rating <- function(df, target_var) {
+            
+            target_var <- enquo(target_var)
+            target_var_char <- as.character(target_var)[2]
+            
+            global_quantiles <- 
+                df %>%
+                select(!!target_var) %>%
+                summarise(
+                    q25 = quantile(!!target_var, 0.25),
+                    q50 = quantile(!!target_var, 0.5),
+                    q75 = quantile(!!target_var, 0.75)
+                ) 
+            
             df %>%
-                mutate(
-                    quality = case_when(
-                        OverallQual %in% c(1, 2, 3, 4) ~ 1,
-                        OverallQual %in% c(9, 10) ~ 6,
-                        TRUE ~ OverallQual - 3
-                    )
-                )
+            gather(var, value, -!!target_var) %>%
+            group_by(var, value) %>%
+            nest %>%
+            mutate(
+                distrib = map(data, ~ecdf(.[[target_var_char]])),
+                rating = map_dbl(distrib, function (cdf) {
+                    prob_rating_1 <- cdf(global_quantiles$q25)
+                    prob_rating_2 <- cdf(global_quantiles$q50) - cdf(global_quantiles$q25)
+                    prob_rating_3 <- cdf(global_quantiles$q75) - cdf(global_quantiles$q50)
+                    prob_rating_4 <- 1 - cdf(global_quantiles$q75)
+                    rating <- prob_rating_1 * 1 + prob_rating_2 * 2 + prob_rating_3 * 3 + prob_rating_4 * 4
+                    rating
+                })
+            ) %>%
+            select(-distrib, -data)
         }
+        
+       rating_transform <- function(data, columns, rating_data) {
+           columns %>% 
+           map_dfc(function (col) {
+               
+               tmp <-
+                   rating_data %>%
+                   filter(var == col) %>%
+                   select(value, rating)
+               
+               mapping <- structure(as.list(tmp$rating), names = as.list(tmp$value))
+               
+               new_col_df <- data_frame(
+                   map(data[, col] [[1]], ~mapping[[.]])
+               )
+               names(new_col_df) <- col
+               new_col_df
+           })
+       }
     })
 })
