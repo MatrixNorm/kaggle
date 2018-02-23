636e6740edea0126fd2e38aca14fa4a460b4cbae
Ky Ky <kyky@kyky.ky>
2018-02-23 22:31:16 +0300

    *

--- R_sources/house_prices/main.R
+++ R_sources/house_prices/main.R
@@ -5,6 +5,7 @@ house_prices <- within(list(), {
     source('./fix_missing_values.R', local = TRUE)
     source('./transform_vars.R', local = TRUE)
     source('./remove_outliers.R', local = TRUE)
+    source('./model_selection.R', local = TRUE)
     # source('./validate.R', local = TRUE)
     # source('./plot.R', local = TRUE)
 })
---
+++ R_sources/house_prices/model_selection.R
@@ -0,0 +1,68 @@
+
+model_selection <- within(list(), 
+{
+    
+    lm <- within(list(), {
+        
+        iterate <- function(base_formula_str, dataset) {
+            base_formula <- as.formula(base_formula_str)
+            target <- all.vars(f)[1]
+            all_predictors <- setdiff(colnames(dataset), target)
+            base_predictors <- labels(terms(base_formula))
+            remaining_predictors <- setdiff(all_predictors, base_predictors)
+            
+            data_frame(
+                base_formula = base_formula_str,
+                var_name = remaining_predictors
+            ) %>%
+                mutate(
+                    formula = paste0(base_formula, ' + ', var_name),
+                    base_r2 = summary(lm(as.formula(base_formula), data=training_dataset))$r.squared
+                ) %>%
+                mutate(    
+                    model = map(formula, ~lm(as.formula(.), data=dataset)),
+                    
+                    r2 = map_dbl(model, function (mod) {
+                        summary(mod)$r.squared
+                    }),
+                    
+                    r2_gain = 100 * (r2 - base_r2) / base_r2,
+                    
+                    max_corr = map2_dbl(base_formula, var_name, function(base_formula, var_name) {
+                        base_predictors <- labels(terms(as.formula(base_formula)))
+                        max(abs(cor(dataset[base_predictors], dataset[var_name])))
+                    })        
+                ) %>%
+                select(-model) %>%
+                filter(r2_gain > 1, max_corr < 0.5) %>%
+                mutate(
+                    r2_gain_adj = r2_gain / (1 + max_corr)
+                ) %>%
+                arrange(desc(r2_gain_adj))
+        }
+        
+        greedy_r2_adj <- function(base_formula_str, dataset) {
+            
+            report <- NULL
+            
+            repeat {
+                
+                result <- iterate(base_formula_str, dataset) 
+                
+                if( nrow(result) < 1 ){
+                    break
+                }
+                
+                base_formula_str <- result[['formula']][[1]]
+                
+                if (is.null(report)) {
+                    report <- result
+                } else {
+                    report <- bind_rows(report, result)
+                }
+            }
+            report
+        }    
+    })
+
+})
