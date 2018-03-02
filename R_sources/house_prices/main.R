9133dab89f77f114d65f5a6db00623790aaa66eb
Ky Ky <kyky@kyky.ky>
2018-03-03 02:58:39 +0300

    *

--- R_sources/house_prices/main.R
+++ R_sources/house_prices/main.R
@@ -4,7 +4,7 @@ house_prices <- within(list(), {
     source('./attributes_selection.R', local = TRUE)
     source('./fix_missing_values.R', local = TRUE)
     source('./transform_vars.R', local = TRUE)
-    source('./remove_outliers.R', local = TRUE)
+    source('./outliers.R', local = TRUE)
     source('./model_selection.R', local = TRUE)
     # source('./validate.R', local = TRUE)
     # source('./plot.R', local = TRUE)
--- rbooks/house_prices/.Rprofile
+++
@@ -1,1 +0,0 @@
-setwd(paste0(Sys.getenv('R_SOURCES'), '/house_prices'))
