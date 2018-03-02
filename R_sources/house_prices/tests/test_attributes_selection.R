d92d7ffc8982f68090edc205bb1a0a8a9fed3643
Ky Ky <kyky@kyky.ky>
2018-03-02 23:04:00 +0300

    *

--- R_sources/house_prices/tests/test_attributes_selection.R
+++ R_sources/house_prices/tests/test_attributes_selection.R
@@ -1,6 +1,9 @@
 
 setwd(paste0(Sys.getenv('R_SOURCES'), '/house_prices'))
 
+library(dplyr, warn.conflicts=FALSE)
+library(purrr, warn.conflicts=FALSE)
+library(tidyr, warn.conflicts=FALSE)
 library(testthat, warn.conflicts=FALSE)
 
 shutup <- within(list(), { 
@@ -12,5 +15,18 @@ shutup <- within(list(), {
     
     test_that("fair coin has entropy of 1", {
         expect_equal(attributes_selection$entropy$entropy(c(0, 1)), 1)
+    })
+    
+    test_that("arrange by entropy # 1", {
+        df <- data_frame(
+            x = c('a', 'a', 'a', 'a'),
+            y = c('a', 'a', 'b', 'b')
+        )
+        expected <- data_frame(
+            var = c('x', 'y'),
+            entropy = c(0, 1)
+        )
+        actual <- attributes_selection$entropy$arrange_vars(df)
+        expect_equal(expected, actual)
     })
 })
--- python_sources/kaggle/house_prices/tests/test_attributes_selection.py
+++ python_sources/kaggle/house_prices/tests/test_attributes_selection.py
@@ -12,3 +12,20 @@ class TestEnropy:
 
     def test_fair_coin2(self):
         assert attr_sel.entropy(pd.Series([0, 1, 0, 1])) == 1
+
+
+class TestArrangeByEntropy:
+    def test_1(self):
+        df = pd.DataFrame({
+            'x': ['a', 'a', 'a', 'a'],
+            'y': ['a', 'a', 'b', 'b'],
+        })
+        expected = pd.DataFrame({
+            'var': ['x', 'y'],
+            'entropy': [0., 1.]
+        })
+        actual = attr_sel.arrange_vars_by_entropy(df)
+        pd.testing.assert_frame_equal(
+            expected.sort_index(axis=1), 
+            actual.sort_index(axis=1)
+        )
