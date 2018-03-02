8c6a078e74b523ac020cdb40f21044cf83b0cb07
Ky Ky <kyky@kyky.ky>
2018-03-02 03:23:21 +0300

    *

--- python_sources/kaggle/house_prices/utils.py
+++ python_sources/kaggle/house_prices/utils.py
@@ -1,7 +1,26 @@
 
 from IPython.display import Image, display
+from rpy2.robjects.lib import grdevices
+
+
+class Plot():
+    def __init__(self, width, height, res):
+        self.width = width
+        self.height = height
+        self.res = res
+        self.result = []
+
+    def __enter__(self):
+        def plot(g):
+            self.result.append(g)
+        return plot
+    
+    def __exit__(self, *args):
+        with grdevices.render_to_bytesio(grdevices.png, width=self.width, height=self.height, res=self.res) as img:
+            print(self.result.pop())
+        display_img(img)
 
 
 def display_img(img):
-    data = img.getvalue()
-    display(Image(data=data, format='png', embed=True))
+    display(Image(data=img.getvalue(), format='png', embed=True))
+
