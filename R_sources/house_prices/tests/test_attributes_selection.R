0ca6b4dea5ddca79ff868be4c06e186992d35658
Ky Ky <kyky@kyky.ky>
2018-03-02 05:32:20 +0300

    *

---
+++ R_sources/house_prices/tests/test_attributes_selection.R
@@ -0,0 +1,6 @@
+
+library(testthat, warn.conflicts=FALSE)
+
+test_that("demo", {
+    expect_equal(1, 1)
+})   
--- ipython_config.py
+++
@@ -1,6 +0,0 @@
-
-c = get_config()
-
-c.InteractiveShellApp.exec_lines = [
-    'import sys; sys.path.append("/mnt/hdd/develop/kaggle/python_sources")'
-]
