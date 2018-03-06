b0c7a0e2f4464c12c0efb07fb020760a8d7f2534
Ky Ky <kyky@kyky.ky>
2018-03-06 03:08:42 +0300

    *

--- python_sources/kaggle/house_prices/missing.py
+++ python_sources/kaggle/house_prices/missing.py
@@ -1,5 +1,7 @@
 
 import pandas as pd
+from helpers import get_character_colnames, get_numeric_colnames
+
 
 colums_with_valid_na = (
     'Alley', 
@@ -45,3 +47,35 @@ def replace_with_most_common(df, columns=None):
     )
     replacement_dict = dict(zip(tmp['var'].tolist(), tmp['value'].tolist()))
     return df.fillna(replacement_dict)
+
+
+def replace_with_value(df, value, columns=None):
+    if columns is None:
+        columns = list(df)
+    return df.fillna(dict((c, 1) for c in columns))
+
+
+def replace_with_zero(df, columns=None):
+    return
+
+
+class FixMissing:
+    def __init__(self, df):
+        self.df = df
+    
+    def replace_with_zero(self):
+        columns = set(get_numeric_colnames(self.df)) - set(['SalePrice'])
+        self.df = replace_with_value(self.df, columns)
+        return self.df
+
+    def fix_valid(self):
+        columns = (set(get_character_colnames(self.df)) & 
+                        set(colums_with_valid_na))
+        self.df = replace_with_value(self.df, '_none_', columns)
+        return self.df
+
+    def replace_with_most_common(self):
+        columns = (set(get_character_colnames(self.df)) - 
+                        set(colums_with_valid_na))
+        self.df = replace_with_most_common(self.df, columns)
+        return self.df
