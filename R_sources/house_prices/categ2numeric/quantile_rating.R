1a06a3b2546eda7d7768ca9ebce04bb675ef336b
Ky Ky <kyky@kyky.ky>
2018-03-08 02:25:54 +0300

    *

--- R_sources/house_prices/categ2numeric/quantile_rating.R
+++ R_sources/house_prices/categ2numeric/quantile_rating.R
@@ -11,41 +11,51 @@ within(list(),
     }
     
     
-    calc_rating_for_selected <- function(df, categ_vars_for_fix, target_var) {
-        
+    calc_rating_for_selected <- function(df, categ_vars, target_var) {
         target_var <- enquo(target_var)
         target_var_char <- as.character(target_var)[2]
         
         df <- 
             df %>%
-            select(categ_vars_for_fix, !!target_var) %>%
+            select(categ_vars, !!target_var) %>%
             filter(!is.na(!!target_var))
         
-        global_quantiles <- 
-            df %>%
-            select(!!target_var) %>%
-            summarise(
-                q25 = quantile(!!target_var, 0.25),
-                q50 = quantile(!!target_var, 0.5),
-                q75 = quantile(!!target_var, 0.75)
-            ) 
+        global_quantiles <- calc_global_quantiles(df, target_var)
         
         df %>%
-            gather(var, value, -!!target_var) %>%
-            group_by(var, value) %>%
-            nest %>%
-            mutate(
-                rating = map_dbl(data, function (df) {
-                    cdf = ecdf(df[[target_var_char]])
-                    prob_rating_1 <- cdf(global_quantiles$q25)
-                    prob_rating_2 <- cdf(global_quantiles$q50) - cdf(global_quantiles$q25)
-                    prob_rating_3 <- cdf(global_quantiles$q75) - cdf(global_quantiles$q50)
-                    prob_rating_4 <- 1 - cdf(global_quantiles$q75)
-                    rating <- prob_rating_1 * 1 + prob_rating_2 * 2 + prob_rating_3 * 3 + prob_rating_4 * 4
-                    rating
-                })
-            ) %>%
-            select(-data) %>% 
-            rbind(list('_global_', '_global_', 0.25*(1+2+3+4)))
+        gather(var, value, -!!target_var) %>%
+        group_by(var, value) %>%
+        nest %>%
+        mutate(
+            rating = map_dbl(data, function (df) {
+                calc_rating(sample, global_quantiles)   
+            })
+        ) %>%
+        select(-data) %>% 
+        rbind(list('_global_', '_global_', 0.25*(1+2+3+4)))
+    }
+    
+    
+    calc_global_quantiles <- function(df, target_var) {
+        target_var <- enquo(target_var)
+        df %>%
+        select(!!target_var) %>%
+        na.omit %>%
+        summarise(
+            q25 = quantile(!!target_var, 0.25),
+            q50 = quantile(!!target_var, 0.5),
+            q75 = quantile(!!target_var, 0.75)
+        )    
+    }
+    
+    
+    calc_rating <- function(sample, global_quantiles) {
+        cdf = ecdf(sample)
+        prob_rating_1 <- cdf(global_quantiles$q25)
+        prob_rating_2 <- cdf(global_quantiles$q50) - cdf(global_quantiles$q25)
+        prob_rating_3 <- cdf(global_quantiles$q75) - cdf(global_quantiles$q50)
+        prob_rating_4 <- 1 - cdf(global_quantiles$q75)
+        rating <- prob_rating_1 * 1 + prob_rating_2 * 2 + prob_rating_3 * 3 + prob_rating_4 * 4
+        rating
     }
 })
---
+++ R_sources/house_prices/tests/test_categ2numeric_quantile_rating.R
@@ -0,0 +1,44 @@
+setwd(paste0(Sys.getenv('R_SOURCES'), '/house_prices'))
+
+devnull <- within(list(), {
+    
+    library(dplyr, warn.conflicts=FALSE)
+    library(purrr, warn.conflicts=FALSE)
+    library(tidyr, warn.conflicts=FALSE)
+    library(testthat, warn.conflicts=FALSE)
+    
+    quantile_rating <- source('./categ2numeric/quantile_rating.R', local = TRUE)$value
+    
+    test_that("calc_global_quantiles: #1", {
+        df <- data_frame(
+            Y = c(10, 12, 20, 22, 30, 32, 40, 42),
+            cat1 = c('a', 'a', 'b', 'b', 'c', 'c', 'd', 'd')
+        )
+        actual <- quantile_rating$calc_global_quantiles(df, Y)
+        expect_true(actual$q25 > 12 & actual$q25 < 20)
+        expect_true(actual$q50 > 20 & actual$q50 < 30)
+        expect_true(actual$q75 > 32 & actual$q75 < 40)
+    })
+    
+    test_that("calc_global_quantiles: NAs are ignored", {
+        df <- data_frame(
+            Y = c(10, 12, NA, 20, 22, 30, NA, 32, 40, 42),
+            cat1 = c('a', 'a', 'a', 'b', 'b', 'c', 'c', 'c', 'd', 'd')
+        )
+        actual <- quantile_rating$calc_global_quantiles(df, Y)
+        expect_true(actual$q25 > 12 & actual$q25 < 20)
+        expect_true(actual$q50 > 20 & actual$q50 < 30)
+        expect_true(actual$q75 > 32 & actual$q75 < 40)
+    })
+    
+    test_that("calc_rating: #1", {
+        calc_rating <- quantile_rating$calc_rating
+        global_quantiles <- list(q25=15, q50=25, q75=35)
+        expect_equal(calc_rating(c(10, 12), global_quantiles), 1)
+        expect_equal(calc_rating(c(16, 20), global_quantiles), 2)
+        expect_equal(calc_rating(c(26, 30), global_quantiles), 3)
+        expect_equal(calc_rating(c(36, 40), global_quantiles), 4)
+        expect_equal(calc_rating(c(15, 16), global_quantiles), 1.5)
+        expect_equal(calc_rating(c(15, 16, 17), global_quantiles), 1 * (1/3) + 2 * (2/3))
+    })
+})
