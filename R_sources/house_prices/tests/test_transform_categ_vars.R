ce5c60dcd1dc44d19bcd4b50ab6f1568b3ed4c46
Ky Ky <kyky@kyky.ky>
2018-03-08 06:51:36 +0300

    *

--- R_sources/house_prices/tests/test_transform_categ_vars.R
+++ R_sources/house_prices/tests/test_transform_categ_vars.R
@@ -12,15 +12,15 @@ devnull <- within(list(), {
     test_that("rating_transform_for_selected: #1", {
         df <- 
             tribble(
-                ~cat1, ~cat2, ~Y,
-                'a',   'x',   10,
-                'a',   'x',   12,
-                'b',   'x',   20,
-                'b',   'x',   22,
-                'c',   'y',   30,
-                'c',   'y',   32,
-                'd',   'y',   40,
-                'd',   'y',   42
+                ~cat1, ~cat2, ~other,
+                'a',   'x',   1,
+                'a',   'x',   2,
+                'b',   'x',   3,
+                'b',   'x',   4,
+                'c',   'y',   5,
+                'c',   'y',   6,
+                'd',   'y',   7,
+                'd',   'y',   8
             )
         ratings <- 
             tribble(
@@ -31,20 +31,62 @@ devnull <- within(list(), {
                 'cat1', 'd',    4,
                 'cat2', 'x',    1.5,
                 'cat2', 'y',    3.5,
-                NA,     NA,     0.25*(1+2+3+4)
+                NA,     NA,     2.5
             )
         actual <- tran_cat$rating_transform_for_selected(df, c('cat1', 'cat2'), ratings)
         expected <- 
             tribble(
-                ~cat1, ~cat2, ~Y,
-                1,   1.5,   10,
-                1,   1.5,   12,
-                2,   1.5,   20,
-                2,   1.5,   22,
-                3,   3.5,   30,
-                3,   3.5,   32,
-                4,   3.5,   40,
-                4,   3.5,   42
+                ~cat1, ~cat2, ~other,
+                1,     1.5,   1,
+                1,     1.5,   2,
+                2,     1.5,   3,
+                2,     1.5,   4,
+                3,     3.5,   5,
+                3,     3.5,   6,
+                4,     3.5,   7,
+                4,     3.5,   8
+            )
+        expect_equal(actual, expected)
+    })
+    
+    test_that("rating_transform_for_selected: unseen values are set to global rating", {
+        df <- 
+            tribble(
+                ~cat1, ~cat2, ~other,
+                'a',   'x',   1,
+                'a',   'x',   2,
+                'b',   'x',   3,
+                'b',   'x',   4,
+                'c',   'y',   5,
+                'c',   'y',   6,
+                'd',   'y',   7,
+                'd',   'y',   8,
+                'f',   'z',   9
+            )
+        ratings <- 
+            tribble(
+                ~var,   ~value, ~rating,
+                'cat1', 'a',    1,
+                'cat1', 'b',    2,
+                'cat1', 'c',    3,
+                'cat1', 'd',    4,
+                'cat2', 'x',    1.5,
+                'cat2', 'y',    3.5,
+                NA,     NA,     2.5
+            )
+        actual <- tran_cat$rating_transform_for_selected(df, c('cat1', 'cat2'), ratings)
+        expected <- 
+            tribble(
+                ~cat1, ~cat2, ~other,
+                1,     1.5,   1,
+                1,     1.5,   2,
+                2,     1.5,   3,
+                2,     1.5,   4,
+                3,     3.5,   5,
+                3,     3.5,   6,
+                4,     3.5,   7,
+                4,     3.5,   8,
+                2.5,   2.5,   9 
             )
         expect_equal(actual, expected)
     })
