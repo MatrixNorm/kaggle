fd8015ee67fcff620eb88e5d135caaf406e589de
Ky Ky <kyky@kyky.ky>
2018-03-12 19:33:25 +0300

    *

--- python_sources/kaggle/house_prices/outliers.py
+++ python_sources/kaggle/house_prices/outliers.py
@@ -4,8 +4,8 @@ condition = 'GrLivArea > 4000 and SalePrice < 2e5'
 
 
 def remove_outliers(dataset):
-    return dataset.query("not(%s)" % condition).reset_index()
+    return dataset.query("not(%s)" % condition).reset_index(drop=True)
 
 
 def get_strange_cases(dataset):
-    return dataset.query(condition).reset_index()
+    return dataset.query(condition).reset_index(drop=True)
