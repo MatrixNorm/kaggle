ba09f873132db6d746bb81cd27f01202ba3406aa
Ky Ky <kyky@kyky.ky>
2018-03-13 17:42:35 +0300

    *

--- R_sources/house_prices/transform_numeric_vars.R
+++ R_sources/house_prices/transform_numeric_vars.R
@@ -1,44 +1,60 @@
 
 within(list(), 
 {
-    calc_tran_config_step1 <- function(dataset, columns, trans) {
-        # returns tibble
-        # var         | x  | log   | sqrt
-        # LotFrontage | 65 | 4.18  | 8.06
+    calc_tran_config_step1 <- function(dataset, trans) {
+        # var	         x	      log	    sqrt   invcube
+        # LotFrontage	65	 4.189655	8.062258   4.020726
+        # LotFrontage	80	 4.394449	8.944272   4.308869
         # ...
-        df1 <-
-            dataset %>%
-            select(columns) %>%
-            select_if(is.numeric) %>%
-            gather(var, x) %>%
-            filter(!is.na(x))
+        df <- dataset %>% gather(var, x)
         
         for (row in 1:nrow(trans)) {
             tran_name <- trans[[row, "tran_name"]]
-            tran_fn <- trans[[row, 'tran_defin']]
-            df1[tran_name] <- tran_fn(df1$x)
+            tran_fn <- trans[[row, 'tran_fn']]
+            df[tran_name] <- tran_fn(df$x)
         }
-        df1
+        df
     }
     
     calc_tran_config_step2 <- function(dataset) {
-        # returns tibble
-        # var          | tran  | L2_distance
-        # BedroomAbvGr | log   | 104.29
-        # BedroomAbvGr | sqrt  | 111.96
-        # BedroomAbvGr | x     | 134.35
+        # var	    tran	value	      value_normed
+        # LotArea	x	    8450.000000	  -0.21639955
+        # LotArea	x	    9600.000000	  -0.06909653
         # ...
+        # LotArea	log	    9.042040	  -0.10174374
+        # ...
+        # LotArea	sqrt	91.923882	  -0.21081495
         dataset %>%
         gather(tran, value, -var) %>%
         group_by(var, tran) %>%
         mutate(
             value_normed = (value - mean(value)) / sd(value)
-        ) %>%
+        )
+    }
+        
+    calc_tran_config_step3 <- function(dataset) {
+        # var	    tran	  value_normed	 k
+        # YrSold    invcube	  -1.3633378	 619
+        # YrSold	invcube	  -0.6027227	 691
+        # ...
+        # YrSold	log	      -1.3634510	 619
+        # ...
+        # YrSold	sqrt	  -1.3632811	 619
+        dataset %>%
         group_by(var, tran, value_normed) %>%
-        arrange(var, tran, value_normed) %>%
         summarise(
             k = n()
         ) %>%
+        arrange(var, tran, value_normed)
+    }
+        
+    calc_tran_config_step4 <- function(dataset) {  
+        # var	    tran	  L2_distance
+        # LotArea	invcube	  10.78862
+        # LotArea	log	      13.31376
+        # LotArea	sqrt	  13.39006
+        # LotArea	x	      50.03278
+        dataset %>%
         mutate(
             empirical = cumsum(k) / sum(k),
             theoretical = pnorm(value_normed),
@@ -50,15 +66,11 @@ within(list(),
         )
     }
     
-    calc_tran_config_step3 <- function(dataset) {
-        # returns tibble
-        # var       | tran | progress_score
-        # GrLivArea | log  | 91.15
-        # X1stFlrSF | log  | 90.77
-        # BsmtUnfSF | sqrt | 79.97
-        # ...
-        #  0 <= progress_score <= 100
-        #
+    calc_tran_config_step5 <- function(dataset) {
+        # var	        best_tran
+        # GrLivArea	    log , 91.1579415746179
+        # LotArea	    invcube , 78.436890821096
+        # OverallQual	log , 29.9443565176069
         dataset %>%
         group_by(var) %>%
         nest %>%
@@ -67,12 +79,21 @@ within(list(),
                 best <- df %>% arrange(L2_distance) %>% head(1)
                 vanilla <- df %>% filter(tran == 'x')
                 progress_score <- 100 * (vanilla$L2_distance - best$L2_distance) / vanilla$L2_distance
-                data_frame(tran = best$tran, progress_score = progress_score)
+                data_frame(tran_name = best$tran, progress_score = progress_score)
             })
         ) %>%
-        select(var, best_tran) %>%
+        select(-data)
+    }
+    
+    calc_tran_config_step6 <- function(dataset) {
+        # var	      tran_name	 progress_score	  tran_fn
+        # GrLivArea	  log	     91.15794157	  function (x) , log(x + 1)
+        # X1stFlrSF	  log	     90.77165686	  function (x) , log(x + 1)
+        # BsmtUnfSF	  sqrt	     79.97603070	  function (x) , sqrt(x)
+        dataset %>%
         unnest(best_tran) %>%
-        filter(tran != 'x') %>%
+        filter(tran_name != 'x') %>%
+        inner_join(Trans, by=c("tran_name")) %>%
         arrange(desc(progress_score))
     }
     
@@ -80,10 +101,12 @@ within(list(),
         if (is.null(columns)) {
             columns <- colnames(dataset)
         }
-        calc_tran_config_step1(dataset, columns, trans) %>%
+        calc_tran_config_step1(dataset, trans) %>%
         calc_tran_config_step2 %>%
         calc_tran_config_step3 %>%
-        inner_join(trans, by=c("tran" = "tran_name"))
+        calc_tran_config_step4 %>%
+        calc_tran_config_step5 %>%
+        calc_tran_config_step6
     }
     
     
