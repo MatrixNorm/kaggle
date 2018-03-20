b3ded1804b7747b9b97dfa357f1535215a33e98b
Ky Ky <kyky@kyky.ky>
2018-03-20 10:25:40 +0300

    *

--- python_sources/kaggle/house_prices/notebook.py
+++ python_sources/kaggle/house_prices/notebook.py
@@ -1,9 +1,13 @@
 
 from IPython.display import Image, display
+from rpy2 import robjects
 from rpy2.robjects import pandas2ri
 from rpy2.robjects.lib import (grdevices, ggplot2)
+from rpy2.robjects.packages import importr
+
 
 pandas2ri.activate()
+gridExtra = importr("gridExtra")
 
 
 class Plot():
@@ -25,6 +29,21 @@ class Plot():
                                          res=self.res) as img:
             print(self.result.pop())
         display_img(img)
+
+
+def grid_plot(*plots, widths=None, layout_matrix=None):
+    
+    layout_matrix = robjects.r['rbind'](
+        *[robjects.IntVector(row) for row in layout_matrix]
+    )
+    
+    with grdevices.render_to_bytesio(grdevices.png, width=2000, height=1200, res=120) as img:
+        gridExtra.grid_arrange(
+            *plots, 
+            widths=widths,
+            layout_matrix=layout_matrix
+        )    
+    display(Image(data=img.getvalue(), format='png', embed=True))
 
 
 def display_img(img):
