bbb0704f122d74cda80730e75407894a3cd9b18f
Ky Ky <kyky@kyky.ky>
2018-03-21 22:54:30 +0300

    *

--- python_sources/kaggle/house_prices/notebook.py
+++ python_sources/kaggle/house_prices/notebook.py
@@ -1,5 +1,5 @@
 
-from IPython.display import Image, display
+from IPython.display import Image, display, HTML
 from rpy2 import robjects
 from rpy2.robjects import pandas2ri
 from rpy2.robjects.lib import (grdevices, ggplot2)
@@ -48,3 +48,31 @@ def grid_plot(plots, widths=None, layout_matrix=None, display_params=None):
 
 def display_img(img):
     display(Image(data=img.getvalue(), format='png', embed=True))
+
+
+def show_table(*dfs, cols=1):
+    wrap = """
+        <div style="display:inline-block;
+                    column-count: {cols}; padding-right:25px;
+                    vertical-align: top;">
+            {df}
+        </div>"""
+    divs = [wrap.format(df=df.to_html(), cols=cols) for df in dfs]
+    return HTML("".join(divs))
+
+
+def show_list(lst):
+    ul_css = "list-style: none; padding-left: 0"
+    li_css = """
+        display: inline-block; 
+        padding: 2px 4px 2px 4px;
+        margin: 3px 3px 3px 3px;
+        background: #efefef;
+        color: #565656;
+        border-radius: 3px
+    """
+    li_template = """<li style="{li_css}">{}</li>"""
+    ul_template = """<ul style="{ul_css}">{}</ul>"""
+    li_html = "".join([li_template.format(item, li_css=li_css) for item in lst])
+    ul_html = ul_template.format(li_html, ul_css=ul_css)
+    display(HTML(ul_html))
